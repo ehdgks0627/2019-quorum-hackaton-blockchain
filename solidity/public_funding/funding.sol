9dad86d26cdab4d44a4379b4a09d71ddcf2dcbc2
tlatkdgus1 <tlatkdgus1@gmail.com>
2019-08-09 15:45:26 +0000

    added public funding

---
+++ solidity/public_funding/funding.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.4.24;
+
+contract QelsToken {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function setTokenPerPrice(bool _isPlus, uint256 _amount) public returns(uint256);
+    function transferFromOwner(address to, uint256 tokens) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+}
+
+contract ElsContract{
+    
+    function abs(uint256 num1, uint256 num2) public pure returns(bool, uint256) {
+        if(num1 < num2) {
+            return (true,  100 - (num1) / (num2/100));
+        } else {
+            return (false, 100 -(num2) / (num1/100));
+        }
+    }
+
+    address owner;
+    address founder;
+    address[] investors;
+    QelsToken token;
+    
+    // 기본 정보
+    string secName; // 증권의 명칭
+    string stockCode; // 증권 코드
+    uint256 startPrice; // 납입일 증권 가격
+//    uint256 expirationPrice; // 만기일 증권 가격
+    uint8  duesPerYear; // 연 이자율
+    uint8 fundingYears; // 투자 기간
+    uint256 issueAmount; // 발행가액
+    uint32 issueDate; // 발행일
+    uint32 expiryDate; // 만기일
+    uint32 underPerPrice; // 하한선  percent
+
+    modifier isExpire{
+        require(block.timestamp > expiryDate);
+        _;
+    }
+    
+    function getInform() public view returns(string, string, uint8, uint256,
+                                         uint32, uint32, uint32) {
+                                            
+        return(secName, stockCode, duesPerYear, issueAmount, issueDate, expiryDate, underPerPrice);     
+        
+    }
+    
+    
+    event QelsBuilt(address founder, address token);
+    event QelsTradeSet(string secName, string stockCode, uint256 startPrice, uint8 duesPerYear, uint256 issueAmout, uint32 issueDate, uint32 expiryDate, uint32 underPerPrice);
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor(address _founder, address _tokenAddr,string _secName, string _stockCode, uint256 _startPrice, uint8 _duesPerYear, uint8 _fundingYears,
+                uint256 _issueAmout, uint32 _issueDate, uint32 _expiryDate, uint32 _underPerPrice) public {
+        owner = msg.sender;
+        founder = _founder;
+        token = QelsToken(_tokenAddr);
+        
+        secName = _secName;
+        stockCode = _stockCode;
+        startPrice = _startPrice;
+        duesPerYear = _duesPerYear;
+        fundingYears = _fundingYears;
+        issueAmount = _issueAmout;
+        issueDate = _issueDate;
+        expiryDate = _expiryDate;
+        underPerPrice = _underPerPrice;
+        
+        emit QelsTradeSet(secName, stockCode, startPrice, duesPerYear, issueAmount, issueDate, expiryDate, underPerPrice);
+        emit QelsBuilt(founder, token);
+    }
+    
+    function investQesl(uint256 _investAmount) public returns(bool){
+        token.transferFromOwner(msg.sender, _investAmount);
+        return true;
+    }
+    
+    function calcDues(uint256 expirationPrice) public isExpire returns(bool) {
+        bool isPlus;
+        uint256 gap;
+        (isPlus, gap) = abs(startPrice, expirationPrice);
+        
+        if(isPlus){                         
+            token.setTokenPerPrice(true, (duesPerYear * fundingYears)); // fund is up
+        }else{
+            //fund is down
+            if(gap >underPerPrice){ // fund is down underPerPrice
+                token.setTokenPerPrice(false, (gap*10)); 
+            }else{
+                token.setTokenPerPrice(true, duesPerYear * fundingYears);
+            }
+        }
+    }
+}
+
---
+++ solidity/public_funding/token.sol
@@ -0,0 +1,139 @@
+pragma solidity ^0.4.24;
+
+library SafeMath {
+    function add(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function sub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function mul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function div(uint a, uint b) internal pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function transfer(address to, uint tokens) public returns (bool success);
+    
+    event Transfer(address indexed from, address indexed to, uint tokens);
+}
+
+contract Owned {
+    address public owner;
+
+    event OwnershipTransfer(address from, address to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyOwnerOrigin {
+        require(tx.origin == owner);
+        _;
+    }
+
+    function transferOwnership(address _owner) public onlyOwner {
+        emit OwnershipTransfer(owner, _owner);
+        owner = _owner;
+    }
+}
+
+contract QelsToken is ERC20Interface, Owned {
+    using SafeMath for uint;
+
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint256 public tokenPerPrice;
+    uint _totalSupply;
+
+    mapping(address => uint) balances;
+    
+    event TokenPerPriceChanged(uint256 tokenPerPrice);
+    
+    function getSymbol() public view returns(string){
+        return symbol;
+    }
+    
+    function getName() public view returns(string){
+        return name;
+    }
+    
+    function getDecimals() public view returns(uint8){
+        return decimals;
+    }
+    
+    function getTokenPerPrice() public view returns(uint256){
+        return tokenPerPrice;
+    }
+    function setTokenPerPrice(bool _isPlus, uint256 _amount) public onlyOwnerOrigin returns(uint256){
+        if(_isPlus){
+            tokenPerPrice.add(_amount);
+        }else{
+            tokenPerPrice.sub(_amount);
+        }
+        
+        emit TokenPerPriceChanged(tokenPerPrice);
+        return tokenPerPrice;
+    }
+
+    constructor(uint256 supply) public {
+        symbol = "Qels";
+        name = "Qels Token";
+        _totalSupply = supply;
+        balances[owner] = supply;
+        tokenPerPrice = 1000;
+        emit TokenPerPriceChanged(tokenPerPrice);
+        emit Transfer(address(0), owner, _totalSupply);
+    }
+
+
+    function totalSupply() public view returns (uint) {
+        return _totalSupply.sub(balances[address(0)]);
+    }
+    
+    
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+
+    function transfer(address to, uint256 tokens) public returns (bool success) {
+        balances[tx.origin] = balances[tx.origin].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(tx.origin, to, tokens);
+        
+        return true;
+    }
+
+    function transferFromOwner(address to, uint256 tokens) public returns (bool){
+        balances[owner] = balances[owner].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(owner, to, tokens);
+        
+        return true;
+    } 
+
+    function () external payable {
+        revert();
+    }
+}
