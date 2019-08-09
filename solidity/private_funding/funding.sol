2bbaf69daea58283ff25227cdb094534c50b235c
tlatkdgus1 <tlatkdgus1@gmail.com>
2019-08-09 15:13:32 +0000

    edited private funding

--- solidity/private_funding/funding.sol
+++ solidity/private_funding/funding.sol
@@ -31,7 +31,6 @@ contract ElsContract{
 //    uint256 expirationPrice; // 만기일 증권 가격
     uint8  duesPerYear; // 연 이자율
     uint8 fundingYears; // 투자 기간
-    uint256 notionalAmount; // 액면금액
     uint256 issueAmount; // 발행가액
     uint32 issueDate; // 발행일
     uint32 expiryDate; // 만기일
@@ -43,15 +42,15 @@ contract ElsContract{
     }
     
     function getInform() public view returns(string, string, uint8, uint256,
-                                        uint256, uint32, uint32, uint32) {
+                                         uint32, uint32, uint32) {
                                             
-        return(secName, stockCode, duesPerYear, notionalAmount, issueAmount, issueDate, expiryDate, underPerPrice);     
+        return(secName, stockCode, duesPerYear, issueAmount, issueDate, expiryDate, underPerPrice);     
         
     }
     
     
     event QelsBuilt(address founder, address investor, address token);
-    event QelsTradeSet(string secName, string stockCode, uint256 startPrice, uint8 duesPerYear, uint256 notionalAmount, uint256 issueAmout, uint32 issueDate, uint32 expiryDate, uint32 underPerPrice);
+    event QelsTradeSet(string secName, string stockCode, uint256 startPrice, uint8 duesPerYear, uint256 issueAmout, uint32 issueDate, uint32 expiryDate, uint32 underPerPrice);
     
     modifier onlyOwner {
         require(msg.sender == owner);
@@ -59,7 +58,7 @@ contract ElsContract{
     }
     
     constructor(address _founder, address _investor, address _tokenAddr,string _secName, string _stockCode, uint256 _startPrice, uint8 _duesPerYear, uint8 _fundingYears,
-                uint256 _notionalAmount, uint256 _issueAmout, uint32 _issueDate, uint32 _expiryDate, uint32 _underPerPrice) public {
+                uint256 _issueAmout, uint32 _issueDate, uint32 _expiryDate, uint32 _underPerPrice) public {
         owner = msg.sender;
         founder = _founder;
         investor = _investor;
@@ -70,14 +69,13 @@ contract ElsContract{
         startPrice = _startPrice;
         duesPerYear = _duesPerYear;
         fundingYears = _fundingYears;
-        notionalAmount = _notionalAmount;
         issueAmount = _issueAmout;
         issueDate = _issueDate;
         expiryDate = _expiryDate;
         underPerPrice = _underPerPrice;
         
         token.transfer(investor, issueAmount);
-        emit QelsTradeSet(secName, stockCode, startPrice, duesPerYear, notionalAmount, issueAmount, issueDate, expiryDate, underPerPrice);
+        emit QelsTradeSet(secName, stockCode, startPrice, duesPerYear, issueAmount, issueDate, expiryDate, underPerPrice);
         emit QelsBuilt(founder, investor, token);
     }
     
